32f41f3a9fb55f92de2426d704e739a0fcb92128
guillaume <guillaumechaulet@hotmail.fr>
2020-05-21 12:15:34 +0200

    first props return

--- quote-may-vuecli/src/App.vue
+++ quote-may-vuecli/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="container">
-    
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
+  import QuoteGrid from './components/QuoteGrid.vue';
   export default {
+    
     data: function(){
       return {
         quotes: [
@@ -13,6 +15,9 @@
         ],
         maxQuotes: 10
       }
+    },
+    components: {
+      appQuoteGrid : QuoteGrid 
     }      
   }
 </script>
