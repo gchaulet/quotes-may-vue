f1ff427ed559aeac8023776c307000963d148097
guillaume <guillaumechaulet@hotmail.fr>
2020-05-21 11:51:32 +0200

    installation of project with components files

---
+++ quote-may-vuecli/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="container">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function(){
+      return {
+        quotes: [
+          'Just a quote to see something'
+        ],
+        maxQuotes: 10
+      }
+    }      
+  }
+</script>
+
+<style>
+
+</style>
