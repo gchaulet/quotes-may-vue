c855fd24f6a8dc2ca6af17a0993c41bf65222daf
guillaume <guillaumechaulet@hotmail.fr>
2020-05-21 15:21:23 +0200

    add quotecount header

--- quote-may-vuecli/src/App.vue
+++ quote-may-vuecli/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a quote to delete it !
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import QuoteGrid from './components/QuoteGrid.vue';
   import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
   export default {
     
     data: function(){
@@ -20,16 +29,27 @@
     },
     methods: {
       newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes) {
+          return alert('Please delete quotes')
+        }
         this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
       }
     },
     components: {
       appQuoteGrid : QuoteGrid, 
-      appNewQuote : NewQuote 
+      appNewQuote : NewQuote,
+      appHeader: Header 
     }      
   }
 </script>
 
 <style>
-
+  #container{
+    width: 80%;
+    margin-right: auto;
+    margin-left: auto;
+  }
 </style>
--- quote-may-vuecli/src/components/NewQuote.vue
+++ quote-may-vuecli/src/components/NewQuote.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="row">
         <form action="">
-            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-12  form-group">
                 <label for="">Quote</label>
-                <textarea class="form-control" name="" id="" cols="30" rows="3" v-model="quote"></textarea>
+                <textarea class="form-control" cols="1000" rows="3" v-model="quote"></textarea>
             </div>    
             <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
                 <button class="btn btn-primary" type="submit" @click.prevent="createNew">Add Quote</button>
@@ -21,6 +21,7 @@ export default {
     },
     methods : {
         createNew () {
+            //it emit to parent
             this.$emit('quoteAdded', this.quote);
             this.quote = '';
         }
